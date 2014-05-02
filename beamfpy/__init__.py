c015767b812082d7ea4bdf8ff115c915ab7f5453
Ennes Sarradj <sarradj@tu-cottbus.de>
2014-05-02 15:03:55 +0000

    header change

--- beamfpy/__init__.py
+++ beamfpy/__init__.py
@@ -8,37 +8,45 @@ ennes.sarradj@gmx.de
 
 A minimal usage example would be:
 
->>    m=MicGeom(from_file='mic_geom.xml')
->>    g=RectGrid(x_min=-0.8, x_max=-0.2, y_min=-0.1, y_max=0.3, z=0.8
-        , increment=0.01)
->>    t1=TimeSamples(name='measured_data.h5')
->>    cal=Calib(from_file='calibration_data.xml')
->>    f1=EigSpectra(time_data=t1, block_size=256, window="Hanning", 
-                    overlap='75%', calib=cal)
->>    e1=BeamformerBase(freq_data=f1, grid=g, mpos=m, r_diag=False)
->>    fr=4000
->>    L1=L_p(e1.synthetic(fr, 0))
+>>>    m = MicGeom(from_file='mic_geom.xml')
+>>>    g = RectGrid(x_min=-0.8, x_max=-0.2, y_min=-0.1, y_max=0.3, z=0.8,\
+ increment=0.01)
+>>>    t1 = TimeSamples(name='measured_data.h5')
+>>>   cal = Calib(from_file='calibration_data.xml')
+>>>    f1 = EigSpectra(time_data=t1, block_size=256, window="Hanning",\
+ overlap='75%', calib=cal)
+>>>    e1 = BeamformerBase(freq_data=f1, grid=g, mpos=m, r_diag=False)
+>>>    fr = 4000
+>>>    L1 = L_p(e1.synthetic(fr, 0))
 
 The classes in the module possess a number of automatic data update
 capabilities. That is, only the traits must be set to get the results.
 The calculation need not be triggered explictely.
+
 The classes are also GUI-aware, they know how to display a graphical user
 interface. So by calling
->>    object_name.configure_traits()
+
+>>>    object_name.configure_traits()
+
 on object "object_name" the relevant traits of each instance object may
 be edited graphically.
+
 The traits could also be set explicitely in the program, either in the
 constructor of an object:
->>    m=MicGeom(from_file='mic_geom.xml')
+
+>>>    m = MicGeom(from_file='mic_geom.xml')
+
 or at a later time
->>    m.from_file='another_mic_geom.xml'
+
+>>>    m.from_file = 'another_mic_geom.xml'
+
 where all objects that depend upon the specific trait will update their
 output if necessary.
 """
 
 __author__ = "Ennes Sarradj, ennes.sarradj@gmx.de"
-__date__ = "2 April 2011"
-__version__ = "3.01beta"
+__date__ = "2 May 2014"
+__version__ = "3.02"
 
 from fileimport import time_data_import, csv_import, td_import, \
 bk_mat_import, datx_import
