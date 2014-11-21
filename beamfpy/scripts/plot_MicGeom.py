b26cbeee9b008c8df547b74617068d3bc01764a1
Ennes Sarradj <sarradj@tu-cottbus.de>
2014-11-21 13:08:29 +0100

    remove unused directories scripts and doc

--- beamfpy/scripts/plot_MicGeom.py
+++
@@ -1,9 +0,0 @@
-from beamfpy import *
-from pylab import *
- 
-m=MicGeom()
-m.configure_traits()
-plot(m.mpos[0],m.mpos[1],'or')
-figure(1,(4,4))
-map(lambda i:text(m.mpos[0,i],m.mpos[1,i],str(i+1)),arange(m.num_mics))
-show()
