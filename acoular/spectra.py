eccd22d8fa97ca11e847e593b367b0c08025c5c3
Tom Jan Gensch <tom.j.gensch@campus.tu-berlin.de>
2018-06-12 18:38:35 +0200

    PowerSpectraEngineOrderAnalyzed as child of PowerSpectra
    Saves a lot of redundance

--- acoular/spectra.py
+++ acoular/spectra.py
@@ -78,7 +78,7 @@ class PowerSpectra( HasPrivateTraits ):
 
     #: FFT block size, one of: 128, 256, 512, 1024, 2048 ... 16384,
     #: defaults to 1024.
-    block_size = Trait(1024, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 
+    block_size = Trait(1024, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536,
         desc="number of samples per FFT block")
 
     #: Index of lowest frequency line to compute, integer, defaults to 1,
@@ -380,161 +380,15 @@ class PowerSpectra( HasPrivateTraits ):
                     [:int(self.block_size/2+1)])
 
 
-class PowerSpectraEngineOrderAnalyzed( HasPrivateTraits ):
+class PowerSpectraEngineOrderAnalyzed( PowerSpectra ):
     """An copy of the PowerSpectra for Engine Ordered analysed signals.
     """
-    #: The :class:`~acoular.sources.SamplesGenerator` object that provides the data.
-    time_data = Trait(SamplesGenerator, 
-        desc="time data object")
-
-    #: Number of samples 
-    numchannels = Delegate('time_data')
-
-    #: The :class:`~acoular.calib.Calib` object that provides the calibration data, 
-    #: defaults to no calibration, i.e. the raw time data is used.
-    #:
-    #: **deprecated**:      use :attr:`~acoular.sources.TimeSamples.calib` property of 
-    #: :class:`~acoular.sources.TimeSamples` objects
-    calib = Instance(Calib)
-
-    #: FFT block size, one of: 128, 256, 512, 1024, 2048 ... 16384,
-    #: defaults to 1024.
-    block_size = Trait(1024, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536,
-        desc="number of samples per FFT block")
-
-    #: Index of lowest frequency line to compute, integer, defaults to 1,
-    #: is used only by objects that fetch the csm, PowerSpectra computes every
-    #: frequency line.
-    ind_low = Range(1, 
-        desc="index of lowest frequency line")
-
-    #: Index of highest frequency line to compute, integer, 
-    #: defaults to -1 (last possible line for default block_size).
-    ind_high = Int(-1, 
-        desc="index of highest frequency line")
-
-    #: Window function for FFT, one of:
-    #:   * 'Rectangular' (default)
-    #:   * 'Hanning'
-    #:   * 'Hamming'
-    #:   * 'Bartlett'
-    #:   * 'Blackman'
-    window = Trait('Rectangular', 
-        {'Rectangular':ones, 
-        'Hanning':hanning, 
-        'Hamming':hamming, 
-        'Bartlett':bartlett, 
-        'Blackman':blackman}, 
-        desc="type of window for FFT")
-
-    #: Overlap factor for averaging: 'None'(default), '50%', '75%', '87.5%'.
-    overlap = Trait('None', {'None':1, '50%':2, '75%':4, '87.5%':8}, 
-        desc="overlap of FFT blocks")
-        
-    #: Flag, if true (default), the result is cached in h5 files and need not
-    #: to be recomputed during subsequent program runs.
-    cached = Bool(True, 
-        desc="cached flag")   
-
-    #: Number of FFT blocks to average, readonly
-    #: (set from block_size and overlap).
-    num_blocks = Property(
-        desc="overall number of FFT blocks")
-
-    #: 2-element array with the lowest and highest frequency, readonly.
-    freq_range = Property(
-        desc = "frequency range" )
-        
-    #: Array with a sequence of indices for all frequencies 
-    #: between :attr:`ind_low` and :attr:`ind_high` within the result, readonly.
-    indices = Property(
-        desc = "index range" )
-        
-    #: Name of the cache file without extension, readonly.
-    basename = Property( depends_on = 'time_data.digest', 
-        desc="basename for cache file")
-
     #: The cross spectral matrix, 
     #: (number of frequencies, numchannels, numchannels) array of complex;
     #: readonly.
     csm = Property( 
         desc="cross spectral matrix")
     
-    #: The floating-number-precision of entries of csm, eigenvalues and 
-    #: eigenvectors, corresponding to numpy dtypes. Default is 64 bit.
-    precision = Trait('complex128', 'complex64', 
-                      desc="precision csm, eva, eve")
-
-    #: Eigenvalues of the cross spectral matrix as an
-    #: (number of frequencies) array of floats, readonly.
-    eva = Property( 
-        desc="eigenvalues of cross spectral matrix")
-
-    #: Eigenvectors of the cross spectral matrix as an
-    #: (number of frequencies, numchannels, numchannels) array of floats,
-    #: readonly.
-    eve = Property( 
-        desc="eigenvectors of cross spectral matrix")
-
-
-
-    # internal identifier
-    digest = Property( 
-        depends_on = ['time_data.digest', 'calib.digest', 'block_size', 
-            'window', 'overlap', 'precision'], 
-        )
-
-    # hdf5 cache file
-    h5f = Instance(tables.File, transient = True )
-    
-    traits_view = View(
-        ['time_data@{}', 
-         'calib@{}', 
-            ['block_size', 
-                'window', 
-                'overlap', 
-                    ['ind_low{Low Index}', 
-                    'ind_high{High Index}', 
-                    '-[Frequency range indices]'], 
-                    ['num_blocks~{Number of blocks}', 
-                    'freq_range~{Frequency range}', 
-                    '-'], 
-                '[FFT-parameters]'
-            ], 
-        ], 
-        buttons = OKCancelButtons
-        )
-    
-    @property_depends_on('time_data.numsamples, block_size, overlap')
-    def _get_num_blocks ( self ):
-        return self.overlap_*self.time_data.numsamples/self.block_size-\
-        self.overlap_+1
-
-    @property_depends_on('time_data.sample_freq, block_size, ind_low, ind_high')
-    def _get_freq_range ( self ):
-        try:
-            return self.fftfreq()[[ self.ind_low, self.ind_high ]]
-        except IndexError:
-            return array([0., 0])
-
-    @property_depends_on( 'block_size, ind_low, ind_high' )
-    def _get_indices ( self ):
-        try:
-            return arange(self.block_size/2+1,dtype=int)[ self.ind_low: self.ind_high ]
-        except IndexError:
-            return range(0)
-
-    @cached_property
-    def _get_digest( self ):
-        return digest( self )
-
-    @cached_property
-    def _get_basename( self ):
-        if 'basename' in self.time_data.all_trait_names():
-            return self.time_data.basename
-        else: 
-            return self.time_data.__class__.__name__ + self.time_data.digest
-
     @property_depends_on('digest')
     def _get_csm ( self ):
         """
@@ -615,81 +469,7 @@ class PowerSpectraEngineOrderAnalyzed( HasPrivateTraits ):
         else:
             return self.h5f.get_node('/', name)
 
-
-
-    @property_depends_on('digest')
-    def _get_eva ( self ):
-        return self._calc_ev()[0]
-
-    @property_depends_on('digest')
-    def _get_eve ( self ):
-        return self._calc_ev()[1]
-
-    def _calc_ev ( self ):
-        """
-        eigenvalues / eigenvectors calculation
-        """
-        name_eva = 'eva_' + self.digest
-        name_eve = 'eve_' + self.digest
-        csm = self.csm #trigger calculation
-        if (not name_eva in self.h5f.root) or (not name_eve in self.h5f.root):
-            csm_shape = self.csm.shape
-            precisionTuple = _precision(self.precision)
-            eva = empty(csm_shape[0:2], dtype=precisionTuple[0])
-            eve = empty(csm_shape, dtype=precisionTuple[1])
-            for i in range(csm_shape[0]):
-                (eva[i], eve[i])=linalg.eigh(self.csm[i])
-            atom_eva = precisionTuple[3]()
-            atom_eve = tables.ComplexAtom(precisionTuple[2])
-            filters = tables.Filters(complevel=5, complib='blosc')
-            ac_eva = self.h5f.create_carray(self.h5f.root, name_eva, atom_eva, \
-                eva.shape, filters=filters)
-            ac_eve = self.h5f.create_carray(self.h5f.root, name_eve, atom_eve, \
-                eve.shape, filters=filters)
-            ac_eva[:] = eva
-            ac_eve[:] = eve
-        return (self.h5f.get_node('/', name_eva), \
-                    self.h5f.get_node('/', name_eve))
-            
-    def synthetic_ev( self, freq, num=0):
-        """Return synthesized frequency band values of the eigenvalues.
-        
-        Parameters
-        ----------
-        freq : float 
-            Band center frequency for which to return the results.
-        num : integer
-            Controls the width of the frequency bands considered; defaults to
-            3 (third-octave band).
-            
-            ===  =====================
-            num  frequency band width
-            ===  =====================
-            0    single frequency line
-            1    octave band
-            3    third-octave band
-            n    1/n-octave band
-            ===  =====================
-
-        Returns
-        -------
-        float 
-            Synthesized frequency band value of the eigenvalues (the sum of
-            all values that are contained in the band).
-        """
-        f = self.fftfreq()
-        if num == 0:
-            # single frequency line
-            return self.eva[searchsorted(f, freq)]
-        else:
-            f1 = searchsorted(f, freq*2.**(-0.5/num))
-            f2 = searchsorted(f, freq*2.**(0.5/num))
-            if f1 == f2:
-                return self.eva[f1]
-            else:
-                return sum(self.eva[f1:f2], 0)
-
-    def fftfreq ( self ):
+    def eofreq ( self ):
         """
         Return the Discrete Fourier Transform sample frequencies in Engine Order.
         
