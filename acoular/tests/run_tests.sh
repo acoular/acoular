d66d5153cac2eea23dc13205b9c98654c4bd747e
Simon Jekosch <s.jekosch@tu-berlin.de>
2019-01-30 11:39:45 +0100

    bash script deletes cache before testing

--- acoular/tests/run_tests.sh
+++ acoular/tests/run_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#activate an enviroment with acoular version to test
+#activate an enviroment with the acoular version to test
 source activate acoular36
+#remove cache data before testing
+rm -rf ./cache/*
 #build a test suite object which runs the tests in this folder
 python -m unittest discover -v -p "testing*.py"
 
