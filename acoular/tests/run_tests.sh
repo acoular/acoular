8f0ea78a517a84bf86b852605ae462170348ec61
Simon Jekosch <s.jekosch@tu-berlin.de>
2019-01-30 16:10:53 +0100

    commenting

--- acoular/tests/run_tests.sh
+++ acoular/tests/run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#activate an enviroment with the acoular version to test
-source activate acoular36
+#if needed activate an enviroment with the acoular version to test
+#source activate acoular36
+
 #remove cache data before testing
 rm -rf ./cache/*
 #build a test suite object which runs the tests in this folder
