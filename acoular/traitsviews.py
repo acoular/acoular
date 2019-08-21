6cbb3986c1c31c046c640a34dc12743ee2e1de33
Ennes Sarradj <ennes.sarradj@gmx.de>
2019-08-21 21:25:18 +0200

    implements traitsviews for Calib

--- acoular/traitsviews.py
+++ acoular/traitsviews.py
@@ -18,33 +18,47 @@ from traitsui.menu import OKCancelButtons
 
 from .microphones import MicGeom
 from .spectra import PowerSpectra
+from .calib import Calib
     
 MicGeom.class_trait_view('traits_view',
                          View(
-                             ['from_file',
-                              'num_mics~',
-                              '|[Microphone geometry]'
-                              ],
-                              buttons = OKCancelButtons
-                              )
-                         )
+                                 ['from_file',
+                                  'num_mics~',
+                                  '|[Microphone geometry]'
+                                  ],
+                                  buttons = OKCancelButtons
+                                  )
+                         )  
                          
 PowerSpectra.class_trait_view('traits_view',
                               View(
-                                ['time_data@{}', 
-                                 'calib@{}', 
-                                    ['block_size', 
+                                      ['time_data@{}', 
+                                       'calib@{}', 
+                                       ['block_size', 
                                         'window', 
                                         'overlap', 
-                                            ['ind_low{Low Index}', 
-                                            'ind_high{High Index}', 
-                                            '-[Frequency range indices]'], 
-                                            ['num_blocks~{Number of blocks}', 
-                                            'freq_range~{Frequency range}', 
-                                            '-'], 
-                                        '[FFT-parameters]'
-                                    ], 
+                                        ['ind_low{Low Index}', 
+                                         'ind_high{High Index}', 
+                                         '-[Frequency range indices]'], 
+                                         ['num_blocks~{Number of blocks}', 
+                                          'freq_range~{Frequency range}', 
+                                          '-'], 
+                                          '[FFT-parameters]'
+                                          ], 
+                                          ], 
+                                          buttons = OKCancelButtons
+                                          )
+                             )
+
+Calib.class_trait_view('traits_view',
+                       View(
+                               ['from_file{File name}', 
+                                ['num_mics~{Number of microphones}', 
+                                 '|[Properties]'
+                                 ]
                                 ], 
+                                title='Calibration data', 
                                 buttons = OKCancelButtons
                                 )
-                              )
+                         )
+                         
