6d9c0926860314252d8349701629fbc77aa061ab
sjekosch <44668203+sjekosch@users.noreply.github.com>
2019-09-02 13:39:24 +0200

    Create reun_test_osx.sh

---
+++ acoular/tests/reun_test_osx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#remove cache data before testing
+rm -rf ./cache/*
+
+#build a test suite object which runs the tests in this folder
+python3 -m unittest discover -v -p "test_*.py"
+
+#remove cache data after testing
+rm -rf ./cache/*
+
