bcebd46e3ccd16e56df02074227a45e4b32def06
Simon Jekosch <s.jekosch@tu-berlin.de>
2019-09-12 15:17:12 +0200

    Update run_tests.sh

--- acoular/tests/run_tests.sh
+++ acoular/tests/run_tests.sh
@@ -6,4 +6,5 @@ rm -rf ./cache/*
 #build a test suite object which runs the tests in this folder
 python -m unittest discover -v -p "test_*.py"
 
-
+#remove cache data after testing
+rm -rf ./cache/*
