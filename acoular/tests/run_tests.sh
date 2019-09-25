5f38cb7deeef6ff9c78802f49830fdd767eb133b
Simon Jekosch <s.jekosch@tu-berlin.de>
2019-09-25 18:10:08 +0200

    typo

--- acoular/tests/run_tests.sh
+++ acoular/tests/run_tests.sh
@@ -6,7 +6,7 @@ rm -rf ./cache/*
 #build a test suite object which runs the tests in this folder
 python -m unittest discover -v -p "test_*.py"
 
-VAL = $?
+VAL=$?
 
 #remove cache data after testing
 rm -rf ./cache/*
