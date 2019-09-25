4cf92ed7a7669d8c9c05a6cd3448589af6138a86
Simon Jekosch <s.jekosch@tu-berlin.de>
2019-09-25 17:46:47 +0200

    shell

--- acoular/tests/run_tests.sh
+++ acoular/tests/run_tests.sh
@@ -5,6 +5,11 @@ rm -rf ./cache/*
 
 #build a test suite object which runs the tests in this folder
 python -m unittest discover -v -p "test_*.py"
+exitvalue = $?
 
 #remove cache data after testing
 rm -rf ./cache/*
+
+exit exitvalue
+
+
