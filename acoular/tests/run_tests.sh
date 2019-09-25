20e0b4ebd55de7546f04ca44b0cffe075c864f57
Simon Jekosch <s.jekosch@tu-berlin.de>
2019-09-25 17:58:47 +0200

    test exitval

--- acoular/tests/run_tests.sh
+++ acoular/tests/run_tests.sh
@@ -5,11 +5,12 @@ rm -rf ./cache/*
 
 #build a test suite object which runs the tests in this folder
 python -m unittest discover -v -p "test_*.py"
-exitvalue = $?
+
+VAL = $?
 
 #remove cache data after testing
 rm -rf ./cache/*
 
-exit exitvalue
+exit $VAL
 
 
