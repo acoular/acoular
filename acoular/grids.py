687bf671460e950f7430296731ba2957da17d9c1
Simon Jekosch <s.jekosch@tu-berlin.de>
2020-07-02 16:25:57 +0200

    new Polygon class and test function

--- acoular/grids.py
+++ acoular/grids.py
@@ -21,7 +21,8 @@
 
 # imports from other packages
 from numpy import mgrid, s_, array, arange, isscalar, absolute, ones, argmin,\
-zeros, where, bool_, nonzero,isclose
+zeros, where,  asfarray,concatenate,sum,ma,ones_like,inf,copysign,fabs
+
 from traits.api import HasPrivateTraits, Float, Property, Any, \
 property_depends_on, cached_property, Bool, List, Instance
 from traits.trait_errors import TraitError
@@ -46,41 +47,163 @@ def in_hull(p, hull, border= True, tol = 0 ):
     else:
         return hull.find_simplex(p,tol = tol)>0
         
-def in_poly(p,poly, border= True, tol = 0.00005 ):
-    """
-    test if points in `p` are in `poly`
-    `p` should be a `Nx2` coordinates of `N` points in `2` dimensions
-    `poly` is the `Mx2` array of the coordinates of `M` points in `2`dimensions.
-        
-    """  
-    tol = tol
-    if border == False:
-        tol = -tol
-    n = len(poly)
-    inside = zeros(len(p[:,0]),bool_)
-    p2x = 0.0
-    p2y = 0.0
-    xints = 0.0
-    p1x,p1y = poly[0]
-    #raycasting loop
-    for i in range(n+1): # +1
-        p2x,p2y = poly[i % n]
-        idx = nonzero((p[:,1] >= min(p1y,p2y)) & (p[:,1] <= max(p1y,p2y)) & (p[:,0]  <= max(p1x,p2x)+ tol))[0] #
-        if not isclose(p1y,p2y):
-            xints = (p[:,1][idx]+tol-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+
+def _det(xvert, yvert):
+    '''Compute twice the area of the triangle defined by points with using
+    determinant formula.
+
+    Input parameters:
+
+    xvert -- A vector of nodal x-coords (array-like).
+    yvert -- A vector of nodal y-coords (array-like).
+
+    Output parameters:
+    Twice the area of the triangle defined by the points.
+
+    Notes:
+
+    _det is positive if points define polygon in anticlockwise order.
+    _det is negative if points define polygon in clockwise order.
+    _det is zero if at least two of the points are concident or if
+        all points are collinear.
+
+    '''
+    xvert = asfarray(xvert)
+    yvert = asfarray(yvert)
+    x_prev = concatenate(([xvert[-1]], xvert[:-1]))
+    y_prev = concatenate(([yvert[-1]], yvert[:-1]))
+    return sum(yvert * x_prev - xvert * y_prev, axis=0)
+
+
+class Polygon:
+    '''Polygon object.
+    Input parameters:
+    x -- A sequence of nodal x-coords.
+    y -- A sequence of nodal y-coords.
+    '''
+
+    def __init__(self, x, y):
+        if len(x) != len(y):
+            raise IndexError('x and y must be equally sized.')
+        self.x = asfarray(x)
+        self.y = asfarray(y)
+        # Closes the polygon if were open
+        x1, y1 = x[0], y[0]
+        xn, yn = x[-1], y[-1]
+        if x1 != xn or y1 != yn:
+            self.x = concatenate((self.x, [x1]))
+            self.y = concatenate((self.y, [y1]))
+        # Anti-clockwise coordinates
+        if _det(self.x, self.y) < 0:
+            self.x = self.x[::-1]
+            self.y = self.y[::-1]
+
+    def is_inside(self, xpoint, ypoint, smalld=1e-12):
+        '''Check if point is inside a general polygon.
+
+        Input parameters:
+        xpoint -- The x-coord of the point to be tested.
+        ypoint -- The y-coords of the point to be tested.
+        smalld -- A small float number.
+
+        xpoint and ypoint could be scalars or array-like sequences.
+
+        Output parameters:
+
+        mindst -- The distance from the point to the nearest point of the
+                  polygon.
+                  If mindst < 0 then point is outside the polygon.
+                  If mindst = 0 then point in on a side of the polygon.
+                  If mindst > 0 then point is inside the polygon.
+
+        Notes:
+        An improved version of the algorithm of Nordbeck and Rydstedt.
+        REF: SLOAN, S.W. (1985): A point-in-polygon program. Adv. Eng.
+             Software, Vol 7, No. 1, pp 45-47.
+
+        '''
+        xpoint = asfarray(xpoint)
+        ypoint = asfarray(ypoint)
+        # Scalar to array
+        if xpoint.shape is tuple():
+            xpoint = array([xpoint], dtype=float)
+            ypoint = array([ypoint], dtype=float)
+            scalar = True
         else:
-            #upper boarder
-            if border == True:
-                idyy = nonzero((p[:,0] > min(p1x, p2x)-tol) & (p[:,0] < max(p1x,p2x)) & (isclose(p[:,1],max(p1y,p2y))))
-                inside[idyy] = ~inside[idyy] 
-        #horizotals    
-        if isclose(p1x,p2x):
-            inside[idx] = ~inside[idx]
-        else:
-            idxx = idx[p[:,0][idx]+tol <= xints]
-            inside[idxx] = ~inside[idxx]  
-        p1x,p1y = p2x,p2y
-    return inside
+            scalar = False
+        # Check consistency
+        if xpoint.shape != ypoint.shape:
+            raise IndexError('x and y has different shapes')
+        # If snear = True: Dist to nearest side < nearest vertex
+        # If snear = False: Dist to nearest vertex < nearest side
+        snear = ma.masked_all(xpoint.shape, dtype=bool)
+        # Initialize arrays
+        mindst = ones_like(xpoint, dtype=float) * inf
+        j = ma.masked_all(xpoint.shape, dtype=int)
+        x = self.x
+        y = self.y
+        n = len(x) - 1  # Number of sides/vertices defining the polygon
+        # Loop over each side defining polygon
+        for i in range(n):
+            d = ones_like(xpoint, dtype=float) * inf
+            # Start of side has coords (x1, y1)
+            # End of side has coords (x2, y2)
+            # Point has coords (xpoint, ypoint)
+            x1 = x[i]
+            y1 = y[i]
+            x21 = x[i + 1] - x1
+            y21 = y[i + 1] - y1
+            x1p = x1 - xpoint
+            y1p = y1 - ypoint
+            # Points on infinite line defined by
+            #     x = x1 + t * (x1 - x2)
+            #     y = y1 + t * (y1 - y2)
+            # where
+            #     t = 0    at (x1, y1)
+            #     t = 1    at (x2, y2)
+            # Find where normal passing through (xpoint, ypoint) intersects
+            # infinite line
+            t = -(x1p * x21 + y1p * y21) / (x21 ** 2 + y21 ** 2)
+            tlt0 = t < 0
+            tle1 = (0 <= t) & (t <= 1)
+            # Normal intersects side
+            d[tle1] = ((x1p[tle1] + t[tle1] * x21) ** 2 +
+                       (y1p[tle1] + t[tle1] * y21) ** 2)
+            # Normal does not intersects side
+            # Point is closest to vertex (x1, y1)
+            # Compute square of distance to this vertex
+            d[tlt0] = x1p[tlt0] ** 2 + y1p[tlt0] ** 2
+            # Store distances
+            mask = d < mindst
+            mindst[mask] = d[mask]
+            j[mask] = i
+            # Point is closer to (x1, y1) than any other vertex or side
+            snear[mask & tlt0] = False
+            # Point is closer to this side than to any other side or vertex
+            snear[mask & tle1] = True
+        if ma.count(snear) != snear.size:
+            raise IndexError('Error computing distances')
+        mindst **= 0.5
+        # Point is closer to its nearest vertex than its nearest side, check if
+        # nearest vertex is concave.
+        # If the nearest vertex is concave then point is inside the polygon,
+        # else the point is outside the polygon.
+        jo = j.copy()
+        jo[j == 0] -= 1
+        area = _det([x[j + 1], x[j], x[jo - 1]], [y[j + 1], y[j], y[jo - 1]])
+        mindst[~snear] = copysign(mindst, area)[~snear]
+        # Point is closer to its nearest side than to its nearest vertex, check
+        # if point is to left or right of this side.
+        # If point is to left of side it is inside polygon, else point is
+        # outside polygon.
+        area = _det([x[j], x[j + 1], xpoint], [y[j], y[j + 1], ypoint])
+        mindst[snear] = copysign(mindst, area)[snear]
+        # Point is on side of polygon
+        mindst[fabs(mindst) < smalld] = 0
+        # If input values were scalar then the output should be too
+        if scalar:
+            mindst = float(mindst)
+        return mindst
 
 
 class Grid( HasPrivateTraits ):
@@ -328,7 +451,10 @@ class RectGrid( Grid ):
             #replaced matplotlib Path by numpy
             #p = Path(array(r).reshape(-1,2))
             #inds = p.contains_points()
-            inds = in_poly(xpos[:2,:].T,array(r).reshape(-1,2))
+            #inds = in_poly(xpos[:2,:].T,array(r).reshape(-1,2))
+            poly = Polygon(array(r).reshape(-1,2)[:,0],array(r).reshape(-1,2)[:,1])
+            dists = poly.is_inside(xpos[0,:],xpos[1,:])   
+            inds = dists >= 0
             for np in arange(self.size)[inds]: # np -- points in x2-circle
                 xi, yi = self.index(xpos[0, np], xpos[1, np])
                 xis += [xi]
@@ -707,7 +833,7 @@ class CircSector( Sector ):
 class PolySector( Sector ):
     """
      Class for defining a polygon sector.
-    
+     
      Can be used for 2D Grids for definining a polygon sector.
     """
     # x1, y1, x2, y2, ... xn, yn :
@@ -733,9 +859,14 @@ class PolySector( Sector ):
             Array indicating which of the given positions lie within the
             given sector                
         """
-        
-        inds = in_poly(pos[:2,:].T, array(self.edges).reshape(-1,2), \
-                           border = self.include_border ,tol = self.abs_tol)
+                
+        poly = Polygon(array(self.edges).reshape(-1,2)[:,0],array(self.edges).reshape(-1,2)[:,1])
+        dists = poly.is_inside(pos[0,:],pos[1,:])   
+        if  self.include_border:
+            inds = dists >= -self.abs_tol
+        else:
+            inds = dists > 0
+            
         
         # if none inside, take nearest
         if ~inds.any() and self.default_nearest:
