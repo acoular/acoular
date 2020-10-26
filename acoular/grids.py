6af5a2d3085d6afcc9e092edf09fba13fb773591
Simon Jekosch <s.jekosch@tu-berlin.de>
2020-10-26 16:21:27 +0100

    new grids

--- acoular/grids.py
+++ acoular/grids.py
@@ -11,6 +11,9 @@
     Grid
     RectGrid
     RectGrid3D
+    ImportGrid
+    LineGrid
+    GridList
     Sector
     RectSector
     CircSector
@@ -21,13 +24,16 @@
 
 # imports from other packages
 from numpy import mgrid, s_, array, arange, isscalar, absolute, ones, argmin,\
-zeros, where,  asfarray,concatenate,sum,ma,ones_like,inf,copysign,fabs
-
+zeros, where,  asfarray,concatenate,sum,ma,ones_like,inf,copysign,fabs ,append,\
+tile,newaxis
+from numpy.linalg import norm
 from traits.api import HasPrivateTraits, Float, Property, Any, \
-property_depends_on, cached_property, Bool, List, Instance
+property_depends_on, cached_property, Bool, List, Instance, File ,on_trait_change,\
+CArray, Tuple, Int
 from traits.trait_errors import TraitError
 #from matplotlib.path import Path
 from scipy.spatial import Delaunay
+from os import path
 from .internal import digest
 
 
@@ -659,6 +665,267 @@ class RectGrid3D( RectGrid):
         return s_[xi1:xi2+1], s_[yi1:yi2+1], s_[zi1:zi2+1]
 
 
+class ImportGrid( Grid ):
+    """
+    Loads a 3D grid from xml file.
+    """
+
+    # internal identifier
+    digest = Property
+    
+    #: Name of the .xml-file from wich to read the data.
+    from_file = File(filter=['*.xml'],
+        desc="name of the xml file to import")
+    
+    
+    gpos_file = CArray(dtype=float,
+        desc="x, y, z position of all Grid Points")
+    
+
+    #: Basename of the .xml-file, without the extension; is set automatically / readonly.
+    basename = Property( depends_on = 'from_file',
+        desc="basename of xml file")
+    
+    @cached_property
+    def _get_basename( self ):
+        return path.splitext(path.basename(self.from_file))[0]
+
+    def _get_digest( self ):
+        return ''
+
+    # 'digest' is a placeholder for other properties in derived classes,
+    # necessary to trigger the depends on mechanism
+    @property_depends_on('basename')
+    def _get_size ( self ):
+        return self.gpos.shape[-1]
+
+    # 'digest' is a placeholder for other properties in derived classes
+    @property_depends_on('basename')
+    def _get_shape ( self ):
+        return self.gpos.shape[-1]
+
+    @property_depends_on('basename')
+    def _get_gpos( self ):
+        return self.gpos_file
+
+
+    subgrids = CArray(
+        desc="names of subgrids for each point")
+
+    
+    @on_trait_change('basename')
+    def import_gpos( self ):
+        """
+        Import the microphone positions from .xml file.
+        Called when :attr:`basename` changes.
+        """
+        if not path.isfile(self.from_file):
+            # no file there
+            self.gpos_file = array([], 'd')
+            return
+        import xml.dom.minidom
+        doc = xml.dom.minidom.parse(self.from_file)
+        names = []
+        xyz = []
+        for el in doc.getElementsByTagName('pos'):
+            names.append(el.getAttribute('subgrid'))
+            xyz.append(list(map(lambda a : float(el.getAttribute(a)), 'xyz')))
+        self.gpos_file = array(xyz, 'd').swapaxes(0, 1)
+        self.subgrids = array(names)
+        
+    def index ( self, x, y ):
+        """
+        Queries the indices for a grid point near a certain co-ordinate.
+
+        This can be used to query results or co-ordinates at/near a certain
+        co-ordinate.
+        
+        Parameters
+        ----------
+        x, y : float
+            The co-ordinates for which the indices are queried.
+
+        Returns
+        -------
+        2-tuple of integers
+            The indices that give the grid point nearest to the given x, y
+            co-ordinates from an array with the same shape as the grid.            
+        """
+        if x < self.x_min or x > self.x_max:
+            raise ValueError("x-value out of range")
+        if y  <  self.y_min or y > self.y_max:
+            raise ValueError("y-value out of range")
+        xi = int((x-self.x_min)/self.increment+0.5)
+        yi = int((y-self.y_min)/self.increment+0.5)
+        return xi, yi
+
+    def indices ( self, *r):
+        """
+        Queries the indices for a subdomain in the grid.
+        
+        Allows either rectangular, circular or polygonial subdomains.
+        This can be used to mask or to query results from a certain 
+        sector or subdomain.
+        
+        Parameters
+        ----------
+        x1, y1, x2, y2, ... : float
+            If three parameters are given, then a circular sector is assumed
+            that is given by its center (x1, y1) and the radius x2.
+            If four paramters are given, then a rectangular sector is
+            assumed that is given by two corners (x1, y1) and (x2, y2). 
+            If more parameters are given, the subdomain is assumed to have
+            polygonial shape with corners at (x_n, y_n).
+
+        Returns
+        -------
+        2-tuple of arrays of integers or of numpy slice objects
+            The indices that can be used to mask/select the grid subdomain from 
+            an array with the same shape as the grid.            
+        """
+        
+        if len(r) == 3: # only 3 values given -> use x,y,radius method
+            xpos = self.gpos
+            xis = []
+            yis = []
+            dr2 = (xpos[0, :]-r[0])**2 + (xpos[1, :]-r[1])**2
+            # array with true/false entries
+            inds = dr2 <= r[2]**2 
+            for np in arange(self.size)[inds]: # np -- points in x2-circle
+                xi, yi = self.index(xpos[0, np], xpos[1, np])
+                xis += [xi]
+                yis += [yi]
+            if not (xis and yis): # if no points in circle, take nearest one
+                return self.index(r[0], r[1])
+            else:
+                return array(xis), array(yis)
+        elif len(r) == 4: # rectangular subdomain - old functionality
+            xi1, yi1 = self.index(min(r[0], r[2]), min(r[1], r[3]))
+            xi2, yi2 = self.index(max(r[0], r[2]), max(r[1], r[3]))
+            return s_[xi1:xi2+1], s_[yi1:yi2+1]
+        else: # use enveloping polygon
+            xpos = self.gpos
+            xis = []
+            yis = []
+            #replaced matplotlib Path by numpy
+            #p = Path(array(r).reshape(-1,2))
+            #inds = p.contains_points()
+            #inds = in_poly(xpos[:2,:].T,array(r).reshape(-1,2))
+            poly = Polygon(array(r).reshape(-1,2)[:,0],array(r).reshape(-1,2)[:,1])
+            dists = poly.is_inside(xpos[0,:],xpos[1,:])   
+            inds = dists >= 0
+            for np in arange(self.size)[inds]: # np -- points in x2-circle
+                xi, yi = self.index(xpos[0, np], xpos[1, np])
+                xis += [xi]
+                yis += [yi]
+            if not (xis and yis): # if no points inside, take nearest to center
+                center = array(r).reshape(-1,2).mean(0)
+                return self.index(center[0], center[1])
+            else:
+                return array(xis), array(yis)
+                #return arange(self.size)[inds]
+
+class LineGrid( Grid ):
+    """
+    Class for Line grid geometries.
+    
+    """
+    
+    #: Staring point of the Grid
+    loc = Tuple((0.0, 0.0, 0.0))
+
+    #: Vector to define the orientation of the line source
+    direction = Tuple((1.0, 0.0, 0.0),
+        desc="Line orientation ")
+    
+    #: Vector to define the length of the line source in meter
+    length = Float(1,desc="length of the line source")
+    
+    #:number of grid points.
+    numpoints = Int(1,desc="length of the line source")
+    
+    #: Overall number of grid points. Readonly; is set automatically when
+    #: other grid defining properties are set
+    size = Property(desc="overall number of grid points")
+
+    #: Grid positions as (3, :attr:`size`) array of floats, without invalid
+    #: microphones; readonly.
+    gpos = Property(desc="x, y, z positions of grid points")
+
+    # internal identifier
+    digest = Property
+
+    def _get_digest( self ):
+        ''
+
+    # 'digest' is a placeholder for other properties in derived classes,
+    # necessary to trigger the depends on mechanism
+    @property_depends_on('numpoints')
+    def _get_size ( self ):
+        return self.gpos.shape[-1]
+
+    # 'digest' is a placeholder for other properties in derived classes
+    @property_depends_on('numpoints')
+    def _get_shape ( self ):
+        return self.gpos.shape[-1]
+
+    @property_depends_on('numpoints,length,direction,loc')
+    def _get_gpos( self ):
+        dist = self.length / self.numpoints 
+        loc = array(self.loc, dtype = float).reshape((3, 1)) 
+        direc_n = self.direction/norm(self.direction)
+        pos = zeros((self.numpoints,3))
+        for s in range(self.numpoints):
+            pos[s] = (loc.T+direc_n*dist*s)
+        return pos.T
+
+
+class GridMerger( Grid ):
+    """
+    Base class for merging different grid geometries.
+    
+    """
+
+    #: List of Grids to be merged
+    #: each grid gets a new subdomain in the new grid
+    #: other grid defining properties are set
+    Grids = List(desc="List of Grids")
+
+    subgrids = Property(desc="names of subgrids for each point")
+    
+    # internal identifier
+    digest = Property
+
+    def _get_digest( self ):
+        return ''
+
+    # 'digest' is a placeholder for other properties in derived classes,
+    # necessary to trigger the depends on mechanism
+    @property_depends_on('digest')
+    def _get_size ( self ):
+        return self.gpos.shape[-1]
+
+    # 'digest' is a placeholder for other properties in derived classes
+    @property_depends_on('digest')
+    def _get_shape ( self ):
+        return self.gpos.shape[-1]
+    
+    @property_depends_on('digest')
+    def _get_subgrids( self ):
+        subgrids = zeros((1,0))
+        for grid in self.Grids:
+            subgrids = append(subgrids,tile(str(grid),grid.size))
+        return subgrids[:,newaxis].T
+    
+
+    @property_depends_on('digest')
+    def _get_gpos( self ):
+        bpos = zeros((3,0))
+        #subgrids = zeros((1,0))
+        for grid in self.Grids:
+            bpos = append(bpos,grid.gpos, axis = 1)
+            #subgrids = append(subgrids,str(grid))
+        return bpos
 
 class Sector( HasPrivateTraits ):
     """
