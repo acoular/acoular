90664baab4e5997e8c54cdc919f1b001cba1d069
esarradj <ennes.sarradj@tu-berlin.de>
2021-01-18 16:12:16 +0100

    adds build script for conda packages

---
+++ build_conda.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# building recipes for currently supported python versions
+conda build --py 3.6 --no-copy-test-source-files recipe.local_py36
+conda build --py 3.7 --no-copy-test-source-files recipe.local_py37
+conda build --py 3.8 --no-copy-test-source-files recipe.local_py38
+
+# convert built Linux packages for all OS
+cd ~/anaconda3/conda-bld/linux-64/
+conda convert -p all -o .. acoular*
+
+
