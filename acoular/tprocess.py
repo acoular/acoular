da59c3ddeb43f66f92869a4296631780b0d34fc7
Art Joshua Robert Pelling <art.jr.pelling@campus.tu-berlin.de>
2021-03-24 23:01:03 +0100

    typo, print debugging...

--- acoular/tprocess.py
+++ acoular/tprocess.py
@@ -2210,7 +2210,7 @@ class TimeConvolve(TimeInOut):
 
         # check if number of kernels matches numchannels
         if self.kernel.shape[1] not in (1, self.source.numchannels):
-            warn("Number of kernels must be either `numchannels` or one.")
+            warn("Number of kernels must be either `numchannels` or one.", stacklevel=2)
 
     @on_trait_change("fft_backend")
     def _change_fft_backend(self):
