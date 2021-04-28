33f0a7013bb779d2428a8f8dba87f1095de878b0
Simon Jekosch <s.jekosch@tu-berlin.de>
2021-04-28 15:53:01 +0200

    add py39 and CI

--- build_conda.sh
+++ build_conda.sh
@@ -4,6 +4,7 @@
 conda build --py 3.6 --no-copy-test-source-files recipe.local_py36
 conda build --py 3.7 --no-copy-test-source-files recipe.local_py37
 conda build --py 3.8 --no-copy-test-source-files recipe.local_py38
+conda build --py 3.9 --no-copy-test-source-files recipe.local_py39
 
 # convert built Linux packages for all OS
 cd ~/anaconda3/conda-bld/linux-64/
