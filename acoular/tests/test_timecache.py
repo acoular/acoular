bd64c19b84842d84892d347958735fdcaa8de359
Mikolaj Czuchaj <mikolaj.czuchaj@tu-berlin.de>
2021-05-05 10:26:01 +0200

    handle old cache files and typos

--- acoular/tests/test_timecache.py
+++ acoular/tests/test_timecache.py
@@ -6,28 +6,28 @@ import unittest
 
 # load some array geometry
 micgeofile = path.join(path.split(bpath)[0], 'xml', 'array_64.xml')
-MICGEOM = MicGeom(from_file=micgeofile) 
+micgeom = MicGeom(from_file=micgeofile)
 
-NUM_SAMPLES = 7
-BLOCK_SIZE = 5
+num_samples = 7
+block_size = 5
 
 class TimeCacheTest(unittest.TestCase):
-    GLOBAL_CACHING_COFIGS= ['individual','all','none','readonly','overwrite']
+    global_caching_configs= ['individual','all','none','readonly','overwrite']
     def test_valid_cache_result(self):
         """
         manually create an incomplete cash file and then read it 
         """
-        for conf in self.GLOBAL_CASHING_COFIGS:
+        for conf in self.global_caching_configs:
             config.global_caching= conf
             with self.subTest(conf):
-                sig = WNoiseGenerator(numsamples=NUM_SAMPLES)
-                ps = PointSource(signal=sig, mics=MICGEOM)
+                sig = WNoiseGenerator(numsamples=num_samples)
+                ps = PointSource(signal=sig, mics=micgeom)
                 tc = TimeCache(source=ps)
-                for i,(block_c, block_nc) in enumerate(zip(tc.result(BLOCK_SIZE), ps.result(BLOCK_SIZE))):
+                for i,(block_c, block_nc) in enumerate(zip(tc.result(block_size), ps.result(block_size))):
                     np.testing.assert_array_almost_equal(block_c,block_nc)
                     if i == 0: break
       
-                for (block_c, block_nc) in zip(tc.result(BLOCK_SIZE), ps.result(BLOCK_SIZE)):
+                for (block_c, block_nc) in zip(tc.result(block_size), ps.result(block_size)):
                     np.testing.assert_array_almost_equal(block_c,block_nc)
 
 
