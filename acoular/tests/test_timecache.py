d492e052b1cecff58a67165aefae526275367720
Mikolaj Czuchaj <mikolaj.czuchaj@tu-berlin.de>
2021-05-05 10:26:01 +0200

    improved test

--- acoular/tests/test_timecache.py
+++ acoular/tests/test_timecache.py
@@ -10,22 +10,25 @@ MICGEOM = MicGeom(from_file=micgeofile)
 
 NUM_SAMPLES = 7
 BLOCK_SIZE = 5
-sig = WNoiseGenerator(numsamples=NUM_SAMPLES)
-ps = PointSource(signal=sig, mics=MICGEOM)
-tc = TimeCache(source=ps)
-
 
 class TimeCacheTest(unittest.TestCase):
-    def test_incompolete_cash(self):
+    GLOBAL_CACHING_COFIGS= ['individual','all','none','readonly','overwrite']
+    def test_valid_cache_result(self):
         """
         manually create an incomplete cash file and then read it 
         """
-        for i,(block_c, block_nc) in enumerate(zip(tc.result(BLOCK_SIZE), ps.result(BLOCK_SIZE))):
-            np.testing.assert_array_almost_equal(block_c,block_nc)
-            if i == 0: break
-  
-        for i,(block_c, block_nc) in enumerate(zip(tc.result(BLOCK_SIZE), ps.result(BLOCK_SIZE))):
-            np.testing.assert_array_almost_equal(block_c,block_nc)
+        for conf in self.GLOBAL_CASHING_COFIGS:
+            config.global_caching= conf
+            with self.subTest(conf):
+                sig = WNoiseGenerator(numsamples=NUM_SAMPLES)
+                ps = PointSource(signal=sig, mics=MICGEOM)
+                tc = TimeCache(source=ps)
+                for i,(block_c, block_nc) in enumerate(zip(tc.result(BLOCK_SIZE), ps.result(BLOCK_SIZE))):
+                    np.testing.assert_array_almost_equal(block_c,block_nc)
+                    if i == 0: break
+      
+                for (block_c, block_nc) in zip(tc.result(BLOCK_SIZE), ps.result(BLOCK_SIZE)):
+                    np.testing.assert_array_almost_equal(block_c,block_nc)
 
 
 if __name__=="__main__":
