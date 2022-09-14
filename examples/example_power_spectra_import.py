470158013358d8384d201389a8de6b45efd68a4a
adamkujawski <kujawski.ad@gmail.com>
2022-09-14 12:54:11 +0200

    adds frequencies trait

--- examples/example_power_spectra_import.py
+++ examples/example_power_spectra_import.py
@@ -11,19 +11,14 @@ from pylab import figure, plot, axis, imshow, colorbar, show, title, subplot
 import numpy as np
 
 # set up the parameters
-sfreq = 51200 
-blocksize = 128
 f = 8000
-fftfreq = linspace(0,sfreq/2,int(blocksize/2+1))
 micgeofile = path.join(path.split(bpath)[0],'xml','array_64.xml')
-
 
 # source positions and rms values of three sources
 loc1=(-0.1,-0.1,0.3) 
 loc2=(0.15,0,0.3) 
 loc3=(0,0.1,0.3)
 rms=array([1,0.7,0.5])
-
 
 # generate test data, in real life this would come from an array measurement
 mg = MicGeom( from_file=micgeofile )
@@ -40,9 +35,9 @@ H_h = H.transpose().conjugate() # H hermetian
 Q = diag(rms)**2 # matrix containing the source strength 
 
 # create full csm
-csm = zeros((int(blocksize/2+1),mg.num_mics,mg.num_mics),dtype=complex)
-csm[fftfreq.searchsorted(f),:,:] = H@Q.astype(complex)@H_h # calculate csm
-ps_import = PowerSpectraImport(csm=csm.copy(), sample_freq=sfreq)
+csm = zeros((1,mg.num_mics,mg.num_mics),dtype=complex)
+csm[0,:,:] = H@Q.astype(complex)@H_h # calculate csm
+ps_import = PowerSpectraImport(csm=csm.copy(), frequencies=f)
 
 bb = BeamformerBase( freq_data=ps_import, steer=st, r_diag=False, cached=False )
 pm = bb.synthetic( f, 0 )
