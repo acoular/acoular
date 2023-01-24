0848818c68ab0faa83e03b7d547ede92196ab9ff
Adam Kujawski <adam.kujawski@tu-berlin.de>
2023-01-24 13:25:55 +0100

    fix exception (return None if undefined source)

---
+++ acoular/tests/fix_test.py
@@ -0,0 +1,7 @@
+from acoular import PowerSpectra, FFTSpectra, PowerSpectraImport, SamplesGenerator
+
+sg = SamplesGenerator(sample_freq=44100)
+ps = PowerSpectra(block_size=4096)
+#ps2 = FFTSpectra()
+#ps3 = PowerSpectraImport()
+
