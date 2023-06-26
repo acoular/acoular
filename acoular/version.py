e3f5fbbc36952cdb5ecec1195fd26cc1a4a91d8f
esarradj <ennes.sarradj@tu-berlin.de>
2023-06-26 11:21:08 +0200

    changes version string to 23.6

--- acoular/version.py
+++ acoular/version.py
@@ -5,5 +5,5 @@
 
 # separate file to find out about version without importing the acoular lib
 __author__ = "Acoular Development Team"
-__date__ = "28 March 2022"
-__version__ = "22.3"
+__date__ = "26 June 2023"
+__version__ = "23.6"
