5e14901411ec2dabcbd469fa15ca682911376500
Adam Kujawski <adam.kujawski@tu-berlin.de>
2023-11-16 14:31:05 +0100

    Remove wrong index and indices method

--- acoular/grids.py
+++ acoular/grids.py
@@ -749,98 +749,6 @@ class ImportGrid( Grid ):
             xyz.append(list(map(lambda a : float(el.getAttribute(a)), 'xyz')))
         self.gpos_file = array(xyz, 'd').swapaxes(0, 1)
         self.subgrids = array(names)
-        
-    def index ( self, x, y ):
-        """
-        Queries the indices for a grid point near a certain co-ordinate.
-
-        This can be used to query results or co-ordinates at/near a certain
-        co-ordinate.
-        
-        Parameters
-        ----------
-        x, y : float
-            The co-ordinates for which the indices are queried.
-
-        Returns
-        -------
-        2-tuple of integers
-            The indices that give the grid point nearest to the given x, y
-            co-ordinates from an array with the same shape as the grid.            
-        """
-        if x < self.x_min or x > self.x_max:
-            raise ValueError("x-value out of range")
-        if y  <  self.y_min or y > self.y_max:
-            raise ValueError("y-value out of range")
-        xi = int((x-self.x_min)/self.increment+0.5)
-        yi = int((y-self.y_min)/self.increment+0.5)
-        return xi, yi
-
-    def indices ( self, *r):
-        """
-        Queries the indices for a subdomain in the grid.
-        
-        Allows either rectangular, circular or polygonial subdomains.
-        This can be used to mask or to query results from a certain 
-        sector or subdomain.
-        
-        Parameters
-        ----------
-        x1, y1, x2, y2, ... : float
-            If three parameters are given, then a circular sector is assumed
-            that is given by its center (x1, y1) and the radius x2.
-            If four paramters are given, then a rectangular sector is
-            assumed that is given by two corners (x1, y1) and (x2, y2). 
-            If more parameters are given, the subdomain is assumed to have
-            polygonial shape with corners at (x_n, y_n).
-
-        Returns
-        -------
-        2-tuple of arrays of integers or of numpy slice objects
-            The indices that can be used to mask/select the grid subdomain from 
-            an array with the same shape as the grid.            
-        """
-        
-        if len(r) == 3: # only 3 values given -> use x,y,radius method
-            xpos = self.gpos
-            xis = []
-            yis = []
-            dr2 = (xpos[0, :]-r[0])**2 + (xpos[1, :]-r[1])**2
-            # array with true/false entries
-            inds = dr2 <= r[2]**2 
-            for np in arange(self.size)[inds]: # np -- points in x2-circle
-                xi, yi = self.index(xpos[0, np], xpos[1, np])
-                xis += [xi]
-                yis += [yi]
-            if not (xis and yis): # if no points in circle, take nearest one
-                return self.index(r[0], r[1])
-            else:
-                return array(xis), array(yis)
-        elif len(r) == 4: # rectangular subdomain - old functionality
-            xi1, yi1 = self.index(min(r[0], r[2]), min(r[1], r[3]))
-            xi2, yi2 = self.index(max(r[0], r[2]), max(r[1], r[3]))
-            return s_[xi1:xi2+1], s_[yi1:yi2+1]
-        else: # use enveloping polygon
-            xpos = self.gpos
-            xis = []
-            yis = []
-            #replaced matplotlib Path by numpy
-            #p = Path(array(r).reshape(-1,2))
-            #inds = p.contains_points()
-            #inds = in_poly(xpos[:2,:].T,array(r).reshape(-1,2))
-            poly = Polygon(array(r).reshape(-1,2)[:,0],array(r).reshape(-1,2)[:,1])
-            dists = poly.is_inside(xpos[0,:],xpos[1,:])   
-            inds = dists >= 0
-            for np in arange(self.size)[inds]: # np -- points in x2-circle
-                xi, yi = self.index(xpos[0, np], xpos[1, np])
-                xis += [xi]
-                yis += [yi]
-            if not (xis and yis): # if no points inside, take nearest to center
-                center = array(r).reshape(-1,2).mean(0)
-                return self.index(center[0], center[1])
-            else:
-                return array(xis), array(yis)
-                #return arange(self.size)[inds]
 
 class LineGrid( Grid ):
     """
@@ -948,7 +856,6 @@ class MergeGrid( Grid ):
             subgrids = append(subgrids,tile(grid.__class__.__name__+grid.digest,grid.size))
         return subgrids[:,newaxis].T
     
-
     @property_depends_on('digest')
     def _get_gpos( self ):
         bpos = zeros((3,0))
