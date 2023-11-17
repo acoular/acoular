79488169bd290fa54cdf57148bef5bbd2c4d99a3
esarradj <ennes.sarradj@tu-berlin.de>
2023-11-17 17:32:51 +0100

    lift version to 23.11

--- acoular/version.py
+++ acoular/version.py
@@ -5,5 +5,5 @@
 
 # separate file to find out about version without importing the acoular lib
 __author__ = "Acoular Development Team"
-__date__ = "26 June 2023"
-__version__ = "23.6"
+__date__ = "20 November 2023"
+__version__ = "23.11"
