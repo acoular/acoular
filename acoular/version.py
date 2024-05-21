f1a5d14fd1dc1a8c6302f56cd4d147688835b75c
Adam Kujawski <adam.kujawski@tu-berlin.de>
2024-05-21 11:36:57 +0200

    adjust version

--- acoular/version.py
+++ acoular/version.py
@@ -4,5 +4,5 @@
 
 # separate file to find out about version without importing the acoular lib
 __author__ = 'Acoular Development Team'
-__date__ = '11 March 2024'
-__version__ = '24.03'
+__date__ = '21 May 2024'
+__version__ = '24.05'
