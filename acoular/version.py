e5da9ef24c128d68254f6608494462f9d4175328
Adam Kujawski <adam.kujawski@tu-berlin.de>
2024-07-22 10:35:09 +0200

    make ready to release

--- acoular/version.py
+++ acoular/version.py
@@ -4,5 +4,5 @@
 
 # separate file to find out about version without importing the acoular lib
 __author__ = 'Acoular Development Team'
-__date__ = '21 May 2024'
-__version__ = '24.05'
+__date__ = '19 July 2024'
+__version__ = '24.07'
