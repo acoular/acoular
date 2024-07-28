e85c7bdeb1b30bbcfbf321d85d3531f114f6324d
Adam Kujawski <adam.kujawski@tu-berlin.de>
2024-07-28 21:11:12 +0200

    adds cross-power spectra

--- examples/io_and_signal_processing_examples/example_spectrogram.py
+++ examples/io_and_signal_processing_examples/example_spectrogram.py
@@ -14,21 +14,21 @@ import numpy as np
 # %%
 # We define two sine wave signals with different frequencies (1000 Hz and 4000 Hz) and a noise signal.
 # Then, the signals are calculated and added together.
-nfreqs = 256
+nfreqs = 257
 sample_freq = 44100
 t_in_s = 30.0
 numsamples = int(sample_freq * t_in_s)
 
-sine1 = ac.SineGenerator(sample_freq=sample_freq, numsamples=numsamples, freq=1000, amplitude=1.0)
+sine1 = ac.SineGenerator(sample_freq=sample_freq, numsamples=numsamples, freq=1000, amplitude=2.0)
 sine2 = ac.SineGenerator(sample_freq=sample_freq, numsamples=numsamples, freq=4000, amplitude=0.5)
 noise = ac.WNoiseGenerator(sample_freq=sample_freq, numsamples=numsamples, rms=0.5)
-mixed_signal = sine1.signal() + sine2.signal() + noise.signal()
-
+mixed_signal = (sine1.signal() + sine2.signal() + noise.signal())[:, np.newaxis]
+#mixed_signal = sine1.signal()[:, np.newaxis].copy()
 
 # %%
 # The mixed signal is then used to create a TimeSamples object.
 
-ts = ac.TimeSamples(data=mixed_signal[:, np.newaxis], sample_freq=sample_freq)
+ts = ac.TimeSamples(data=mixed_signal, sample_freq=sample_freq)
 print(ts.numsamples, ts.numchannels)
 
 
@@ -39,27 +39,32 @@ print(ts.numsamples, ts.numchannels)
 # To do so, we use the :class:`acoular.fprocess.FFT` class, which calculates the FFT spectra of the signal blockwise.
 
 fft = ac.RFFT(source=ts)
+spec = next(fft.result(num=nfreqs))
+blocksize = fft.get_blocksize(nfreqs)
+time_block = next(ts.result(num=blocksize))
+print("Parseval's theorem:")
+print("signal energy in time domain", np.sum(time_block**2))
+print("signal energy in frequency domain", (1/blocksize*np.sum(spec*spec.conjugate())).real)
+
 tp = ac.Power(source=fft)  # results in the power spectrum
 spectrogram = ac.tools.return_result(tp, num=nfreqs, concat=False)[:, :, 0]  # power spectra for the first channel
 
 # %%
 # Plot the spectrogram of the signal for different number of averages.
-
+# Here, we plot the amplitude spectra for each time block.
 import matplotlib.pyplot as plt
 
 # plot the power spectrogram
 plt.figure()
 plt.imshow(
-    ac.L_p(spectrogram).T,
+    np.abs(spectrogram.T) / blocksize,
     origin='lower',
     aspect='auto',
     extent=(0, t_in_s, 0, sample_freq / 2),
-    vmax=ac.L_p(spectrogram).max(),
-    vmin=ac.L_p(spectrogram).max() - 40,
 )
 plt.xlabel('Time / s')
 plt.ylabel('Frequency / Hz')
-plt.colorbar(label='Power Spectrum / dB')
+plt.colorbar(label='Power Spectrum')
 plt.show()
 
 # %%
@@ -76,7 +81,6 @@ plt.figure()
 for navg in [0, 10, 100, None]:
     tavg.numaverage = navg
     spectrum = next(tavg.result(num=nfreqs))
-    print(spectrum.sum())
     Lm = ac.L_p(spectrum[:, 0])
     freqs = fft.fftfreq(numfreq=nfreqs)
     plt.plot(freqs, Lm, label=f'navg = {navg}')
@@ -86,4 +90,42 @@ plt.grid()
 plt.legend()
 plt.semilogx()
 plt.show()
+# %%
+# Create a cross-spectral matrix for multichannel signals
+# --------------------------------------------------------
+# To calculate the cross-spectral matrix of the signal, we use the :class:`acoular.fprocess.CrossPowerSpectra` class.
+# First, we create a TimeSamples object with two channels.
+#numsamples=sample_freq*5
+#noise = ac.WNoiseGenerator(sample_freq=sample_freq, numsamples=numsamples, rms=1.0).signal()[:, np.newaxis]
+
+ts = ac.TimeSamples(data=np.concatenate([mixed_signal, 0.5*mixed_signal], axis=1),
+                    sample_freq=sample_freq)
+
+avg_csm = ts > fft > ac.CrossPowerSpectra() > ac.BlockAverage()
+csm = next(avg_csm.result(num=nfreqs))
+
+# reshape the cross-spectral matrix to a 3D array of shape (numfreq, numchannels, numchannels)
+csm = csm.reshape(nfreqs, ts.numchannels, ts.numchannels)
+
+# compare with PowerSpectra
+csm_comp = ac.PowerSpectra(source=ts, block_size=(nfreqs-1)*2, cached=False).csm[:,:,:]
+
+
+#%%
+# compare with PowerSpectra
+plt.figure()
+for i in range(ts.numchannels):
+    auto_pow_spectrum = csm_comp[:, i, i].real
+    plt.plot(fft.fftfreq(numfreq=nfreqs), auto_pow_spectrum, label=f'PowerSpectra Channel {i}')
+    auto_pow_spectrum = csm[:, i, i].real
+    plt.plot(fft.fftfreq(numfreq=nfreqs), auto_pow_spectrum, label=f'Channel {i}')
+plt.xlabel('Frequency / Hz')
+plt.ylabel('Power Spectrum')
+plt.grid()
+plt.legend()
+plt.semilogx()
+plt.show()
+
+
+
 # %%
