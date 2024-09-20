94b09431178b3fe55a23a33b2e120cf8e9b77b24
Gert Herold <g.j.h@gmx.net>
2024-09-20 17:09:18 +0200

    astype -> asarray

--- acoular/grids.py
+++ acoular/grids.py
@@ -33,6 +33,7 @@ from numpy import (
     arange,
     argmin,
     array,
+    asarray,
     concatenate,
     copysign,
     fabs,
@@ -110,8 +111,8 @@ def _det(xvert, yvert):
         all points are collinear.
 
     """
-    xvert = xvert.astype(float)
-    yvert = yvert.astype(float)
+    xvert = asarray(xvert, dtype=float)
+    yvert = asarray(yvert, dtype=float)
     x_prev = concatenate(([xvert[-1]], xvert[:-1]))
     y_prev = concatenate(([yvert[-1]], yvert[:-1]))
     return sum(yvert * x_prev - xvert * y_prev, axis=0)
@@ -128,8 +129,8 @@ class Polygon:
         if len(x) != len(y):
             msg = 'x and y must be equally sized.'
             raise IndexError(msg)
-        self.x = x.astype(float)
-        self.y = y.astype(float)
+        self.x = asarray(x, dtype=float)
+        self.y = asarray(y, dtype=float)
         # Closes the polygon if were open
         x1, y1 = x[0], y[0]
         xn, yn = x[-1], y[-1]
@@ -166,8 +167,8 @@ class Polygon:
              Software, Vol 7, No. 1, pp 45-47.
 
         """
-        xpoint = xpoint.astype(float)
-        ypoint = ypoint.astype(float)
+        xpoint = asarray(xpoint, dtype=float)
+        ypoint = asarray(ypoint, dtype=float)
         # Scalar to array
         if xpoint.shape == ():
             xpoint = array([xpoint], dtype=float)
