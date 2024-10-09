8d73d74d56df61cfb56d5401f3daf1a313b15a90
Adam Kujawski <adam.kujawski@tu-berlin.de>
2024-10-09 09:52:35 +0200

    all classes

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,15 +1,40 @@
 # ------------------------------------------------------------------------------
 # Copyright (c) Acoular Development Team.
 # ------------------------------------------------------------------------------
-"""Implements testing of frequency beamformers."""
+"""Tests if all Acoular classes can be instatiated and if important traits can be set without errors."""
 
+import importlib
+import inspect
+import pkgutil
+import tempfile
 import warnings
 
-import acoular as ac
 import pytest
-from traits.api import Bool, Enum, Float, HasTraits, Int, Range, TraitEnum
+from traits.api import Bool, Enum, Float, Int, Range, TraitEnum
 
-all_classes = dir(ac)
+
+def get_all_classes():
+    classes = []
+    package = importlib.import_module('acoular')
+    for module_info in pkgutil.walk_packages(package.__path__, package.__name__ + '.'):
+        module = importlib.import_module(module_info.name)
+        for _, obj in inspect.getmembers(module, inspect.isclass):
+            if obj.__module__ == module_info.name:  # ensure class is defined in the current module
+                classes.append(obj)
+    return classes
+
+
+def create_instance(acoular_cls):
+    if acoular_cls.__name__ in ['H5CacheFileH5py', 'H5CacheFileTables', 'H5FileH5py', 'H5FileTables']:
+        return acoular_cls(tempfile.mkstemp()[1] + '.h5', 'w')
+    if acoular_cls.__name__ in ['LockedGenerator', 'LazyBfResult']:
+        return acoular_cls(None)
+    if acoular_cls.__name__ == 'Polygon':
+        return acoular_cls([0], [1])
+    return acoular_cls()
+
+
+all_classes = get_all_classes()
 
 
 @pytest.mark.parametrize('acoular_cls', all_classes)
@@ -17,10 +42,7 @@ def test_instancing(acoular_cls):
     """test that all Acoular classes can be instatiated."""
     with warnings.catch_warnings():
         warnings.simplefilter('ignore', category=DeprecationWarning)
-        # iterate over all Acoular definitions labels
-        j = getattr(ac, acoular_cls)  # class, function or variable
-        if isinstance(j, type):  # is this a class ?
-            j()  # this is an instance of the class
+        create_instance(acoular_cls)
 
 
 @pytest.mark.parametrize('acoular_cls', all_classes)
@@ -28,16 +50,13 @@ def test_set_traits(acoular_cls):
     """test that important traits can be set."""
     with warnings.catch_warnings():
         warnings.simplefilter('ignore', category=DeprecationWarning)
-        # iterate over all Acoular definitions labels
-        j = getattr(ac, acoular_cls)  # class, function or variable
-        # HasTraits derived class ?
-        if isinstance(j, type) and issubclass(j, HasTraits) and ('digest' in j.class_traits()):
-            do = j.class_traits()['digest'].depends_on
+        if hasattr(acoular_cls, 'class_traits') and 'digest' in acoular_cls.class_traits():
+            do = acoular_cls.class_traits()['digest'].depends_on
             if do:
-                obj = j()
+                obj = create_instance(acoular_cls)
                 for k in do:
-                    if k in j.class_trait_names():
-                        tr = j.class_traits()[k]
+                    if k in acoular_cls.class_trait_names():
+                        tr = acoular_cls.class_traits()[k]
                         # handling different Trait types
                         # TODO: use hypothesis based setattr
                         if tr.is_trait_type(Int):
