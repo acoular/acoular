4ffe60d040d6c8fb8e0474f160f228af741daa2e
Gert Herold <g.j.h@gmx.net>
2024-11-29 10:04:04 +0100

    - adds readonly option for deprecated aliases - allow multiple alias entries in dictionary

--- acoular/deprecation.py
+++ acoular/deprecation.py
@@ -29,7 +29,7 @@ class DeprecatedName(HasTraits):
         self.file = name
 
 
-def deprecated_alias(old2new):
+def deprecated_alias(old2new, read_only=False):
     """Decorator function for deprecating renamed class traits.
     Replaced traits should no longer be part of the class definition itself
     and only mentioned in this decorator's parameter list.
@@ -41,6 +41,10 @@ def deprecated_alias(old2new):
     old2new: dict
         Dictionary containing the deprecated trait names as keys and
         their new names as values.
+    read_only: bool or list
+        If True, all deprecated traits will be "read only".
+        If False (default), all deprecated traits can be read and set.
+        If list, traits whose names are in list will be "read only".
     """
 
     def decorator(cls):
@@ -48,6 +52,23 @@ def deprecated_alias(old2new):
 
         class LocalAliasHost(cls):
             """Dummy class for adding traits."""
+
+            def _alias_accessor_factory(self, old, new, readonly=False):
+                """Function to define setter and getter routines for alias property trait."""
+                msg = f"Deprecated use of '{old}' trait. Please use the '{new}' trait instead."
+
+                def getter(self):
+                    warn(msg, DeprecationWarning, stacklevel=2)
+                    return getattr(self, new)
+
+                if readonly:
+                    return (getter,)
+
+                def setter(self, value):
+                    warn(msg, DeprecationWarning, stacklevel=2)
+                    setattr(self, new, value)
+
+                return (getter, setter)
 
             def __init__(self, **traits):
                 # Split init arguments into known and unknown traits:
@@ -66,18 +87,12 @@ def deprecated_alias(old2new):
                 # Add deprecated traits to object properties and link them to
                 # the new ones with a deprecation warning.
                 for old, new in old2new.items():
-                    if new in self.class_traits():  # Check if the new trait exists
-                        msg = f"Deprecated use of '{old}' trait. Please use the '{new}' trait instead."
+                    if new in self.class_traits():
+                        # Set "read only" status depending on global read_only argument
+                        current_read_only = (old in read_only) if isinstance(read_only, list) else read_only
 
-                        def getter(self, msg=msg, new=new):
-                            warn(msg, DeprecationWarning, stacklevel=2)
-                            return getattr(self, new)
-
-                        def setter(self, value, msg=msg, new=new):
-                            warn(msg, DeprecationWarning, stacklevel=2)
-                            setattr(self, new, value)
-
-                        self.add_trait(old, Property(getter, setter))
+                        # If the new trait exists, set alias
+                        self.add_trait(old, Property(*self._alias_accessor_factory(old, new, current_read_only)))
 
                     else:
                         error_msg = f"Cannot create trait '{old}' because its replacement trait '{new}' does not exist."
