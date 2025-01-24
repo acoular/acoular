c8e991b33f147c8c99421a2676daf55c65ff42a2
Art Pelling <a.pelling@tu-berlin.de>
2025-01-24 09:40:37 +0100

    Test traits dependencies (#374)
    * change misleading varnames
    
    * expand get_all_classes
    
    * add basic test
    
    * depends_on arg is list of strings
    
    fix bug
    
    another one
    
    * fix test
    
    * whats new
    
    * filter warnings in create_instance
    
    * xfail for PowerSpectraImport
    
    * don't skip Any
    
    * turn FlowField into an abstract class
    
    * xfail MergeGrid
    
    * fix linting
    
    * fix tests
    
    * fixup! fix linting
    
    * remove FlowField from regression test cases
    
    * lint todos
    
    * improve docstring
    
    * improve what's new
    
    * Update acoular/environments.py
    
    Co-authored-by: adku <adam.kujawski@tu-berlin.de>
    
    * remove MergeGrid again
    
    * remove try except clause
    
    ---------
    
    Co-authored-by: adku <adam.kujawski@tu-berlin.de>

--- tests/unittests/test_classes.py
+++ tests/unittests/test_classes.py
@@ -13,18 +13,21 @@ from traits.api import Bool, Enum, Float, Int, Range, TraitEnum
 from tests.utils import get_all_classes
 
 all_classes = get_all_classes()
+all_hastraits_classes = get_all_classes(hastraits_only=True)
 
 
 def create_instance(acoular_cls):
-    if isabstract(acoular_cls):
-        pytest.skip(f'{acoular_cls.__name__} is an abstract base class')
-    if acoular_cls.__name__ in ['H5CacheFileH5py', 'H5CacheFileTables', 'H5FileH5py', 'H5FileTables']:
-        return acoular_cls(tempfile.mkstemp()[1] + '.h5', 'w')
-    if acoular_cls.__name__ in ['LockedGenerator', 'LazyBfResult']:
-        return acoular_cls(None)
-    if acoular_cls.__name__ == 'Polygon':
-        return acoular_cls([0], [1])
-    return acoular_cls()
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', category=DeprecationWarning)
+        if isabstract(acoular_cls):
+            pytest.skip(f'{acoular_cls.__name__} is an abstract base class.')
+        if acoular_cls.__name__ in ['H5CacheFileH5py', 'H5CacheFileTables', 'H5FileH5py', 'H5FileTables']:
+            return acoular_cls(tempfile.mkstemp()[1] + '.h5', 'w')
+        if acoular_cls.__name__ in ['LockedGenerator', 'LazyBfResult']:
+            return acoular_cls(None)
+        if acoular_cls.__name__ == 'Polygon':
+            return acoular_cls([0], [1])
+        return acoular_cls()
 
 
 @pytest.mark.parametrize('acoular_cls', all_classes)
@@ -35,33 +38,78 @@ def test_instancing(acoular_cls):
         create_instance(acoular_cls)
 
 
-@pytest.mark.parametrize('acoular_cls', all_classes)
+@pytest.mark.parametrize('acoular_cls', all_hastraits_classes)
 def test_set_traits(acoular_cls):
     """Test that important traits can be set."""
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', category=DeprecationWarning)
-        if hasattr(acoular_cls, 'class_traits') and 'digest' in acoular_cls.class_traits():
-            do = acoular_cls.class_traits()['digest'].depends_on
-            if do:
-                obj = create_instance(acoular_cls)
-                digest = obj.digest
-                assert digest is not None
-                assert digest != ''
-                for k in do:
-                    if k in acoular_cls.class_trait_names():
-                        tr = acoular_cls.class_traits()[k]
-                        # handling different Trait types
-                        # TODO: use hypothesis based setattr #noqa: TD002, TD003, FIX002
-                        if tr.is_trait_type(Int):
-                            setattr(obj, k, 1)
-                        elif tr.is_trait_type(Float):
-                            setattr(obj, k, 0.1)
-                        elif tr.is_trait_type(Bool):
-                            setattr(obj, k, False)
-                        elif tr.is_trait_type(Range):
-                            low = tr.handler._low
-                            high = tr.handler._high
-                            setattr(obj, k, (high + low) / 2)
-                        elif tr.is_trait_type(TraitEnum) or tr.is_trait_type(Enum):
-                            v = tr.handler.values
-                            setattr(obj, k, v[len(v) // 2])
+    if hasattr(acoular_cls, 'class_traits') and 'digest' in acoular_cls.class_traits():
+        do = acoular_cls.class_traits()['digest'].depends_on
+        if do:
+            obj = create_instance(acoular_cls)
+            digest = obj.digest
+            assert digest is not None
+            assert digest != ''
+            for k in do:
+                if k in acoular_cls.class_trait_names():
+                    tr = acoular_cls.class_traits()[k]
+                    # handling different Trait types
+                    # TODO(adku1173): use hypothesis based setattr
+                    # 421
+                    if tr.is_trait_type(Int):
+                        setattr(obj, k, 1)
+                    elif tr.is_trait_type(Float):
+                        setattr(obj, k, 0.1)
+                    elif tr.is_trait_type(Bool):
+                        setattr(obj, k, False)
+                    elif tr.is_trait_type(Range):
+                        low = tr.handler._low
+                        high = tr.handler._high
+                        setattr(obj, k, (high + low) / 2)
+                    elif tr.is_trait_type(TraitEnum) or tr.is_trait_type(Enum):
+                        v = tr.handler.values
+                        setattr(obj, k, v[len(v) // 2])
+
+
+# TODO(artpelling): remove the entries when respective issue is fixed
+# 418
+# 420
+xfails = {
+    'PowerSpectraImport': 'Issue #418',
+    'CsmAIAABenchmark': 'Issue #418',
+    'SpatialInterpolator': 'Issue #420',
+}
+for c in all_hastraits_classes.copy():
+    try:
+        if c.__name__ in xfails:
+            all_hastraits_classes.remove(c)
+            all_hastraits_classes.append(pytest.param(c, marks=pytest.mark.xfail(reason=xfails[c.__name__])))
+    except AttributeError:
+        pass
+
+
+@pytest.mark.parametrize('acoular_cls', all_hastraits_classes)
+def test_trait_dependencies(acoular_cls):
+    """Assert that any property that is being depended on itself depends on something."""
+
+    def check_or_unpack(obj, tname, toptrait, toplevel=True):
+        assert len(objtname := tname.split('.')) < 3, 'Trait dependency too deep.'
+        # recurse if trait depends on a trait of another object
+        if len(objtname) == 2:
+            # instantiate the trait
+            obj = create_instance(obj.trait(objtname[0]).trait_type.klass)
+            tname = objtname[1]
+            check_or_unpack(obj, tname, toptrait, toplevel=False)
+        else:
+            # deal with traits extended name definition by stripping trailing '[]'
+            # https://docs.enthought.com/traits/traits_user_manual/listening.html#semantics
+            tname = tname.replace('[]', '')
+            trait = obj.trait(tname)
+            if trait.is_property:
+                if trait.depends_on is None:
+                    assert toplevel, f'{toptrait} depends on {tname}, but {tname} does not depend on anything.'
+                else:
+                    for otname in trait.depends_on:
+                        check_or_unpack(obj, otname, toptrait, toplevel=False)
+
+    obj = create_instance(acoular_cls)
+    for tname in obj.traits():
+        check_or_unpack(obj, tname, tname)
