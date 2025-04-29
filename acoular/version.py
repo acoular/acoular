25814a7ddac6fe537ee05fce63d54b3076cc281b
adku <adam.kujawski@tu-berlin.de>
2025-04-29 13:25:22 +0200

    change version, update what's new and cff, fix conda build (#466)
    * change version, update what's new
    
    * use export
    
    * update cff

--- acoular/version.py
+++ acoular/version.py
@@ -4,5 +4,5 @@
 
 # separate file to find out about version without importing the acoular lib
 __author__ = 'Acoular Development Team'
-__date__ = '18 March 2025'
-__version__ = '25.03.post1'
+__date__ = '29 April 2025'
+__version__ = '25.04'
