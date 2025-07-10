19fc74f986f23bacc5bb97b15c1d520c78dcc242
Theo <51700428+jtodev@users.noreply.github.com>
2025-07-10 15:44:41 +0200

    Update examples/introductory_examples/example_sectors.py
    Co-authored-by: Art Pelling <a.pelling@tu-berlin.de>

--- examples/introductory_examples/example_sectors.py
+++ examples/introductory_examples/example_sectors.py
@@ -103,7 +103,7 @@ plt.show()
 # =======================
 #
 # Acoular uses sector classes to define regions of interest in the grid. Each sector
-# class provides a method called :meth:`~acoular.grids.Sector.contains` that takes a
+# class provides a :meth:`~acoular.grids.Sector.contains` method that takes a
 # grid's :attr:`~acoular.grids.Grid.pos` attribute as parameter and checks which grid
 # points are inside the sector.
 #
