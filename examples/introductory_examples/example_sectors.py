ef5975eee25e0f1f8b1a3c576c5de1490f63772a
Theo <51700428+jtodev@users.noreply.github.com>
2025-07-10 15:43:53 +0200

    Update examples/introductory_examples/example_sectors.py
    Co-authored-by: Art Pelling <a.pelling@tu-berlin.de>

--- examples/introductory_examples/example_sectors.py
+++ examples/introductory_examples/example_sectors.py
@@ -60,7 +60,7 @@ bb = ac.BeamformerBase(freq_data=f, steer=st)
 #
 # Let us start by defining a sector.
 #
-# Think of a sector as a region in the sound map where you want to focus your analysis.
+# One can think of a sector as a region in the sound map to which the analysis is constrained.
 #
 # Here, we will create a rectangular sector that covers the trailing edge of the airfoil.
 # This is often where interesting noise sources are found in aeroacoustic measurements.
