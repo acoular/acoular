35629ceee5a09c9f3a4dba4bdbe4485635b7a96e
Theo <51700428+jtodev@users.noreply.github.com>
2025-07-10 15:38:09 +0200

    Update tests/unittests/test_grid.py
    Co-authored-by: Art Pelling <a.pelling@tu-berlin.de>

--- tests/unittests/test_grid.py
+++ tests/unittests/test_grid.py
@@ -100,4 +100,4 @@ def test_export_gpos_all_grids(grid, tmp_path):
     export_file = tmp_path / f'test_gpos_{grid.digest}.xml'
     grid.export_gpos(export_file)
     new_grid = ac.ImportGrid(file=export_file)
-    assert (grid.pos == new_grid.pos).all()
+    assert array_equal(grid.pos, new_grid.pos)
