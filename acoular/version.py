8978d705f7ca33c56fe4496b02be456d569be05c
Art Pelling <a.pelling@tu-berlin.de>
2026-01-28 10:56:28 +0100

    today is today not yesterday

--- acoular/version.py
+++ acoular/version.py
@@ -5,5 +5,5 @@
 """Dedicated file to determine the package version without importing acoular."""
 
 __author__ = 'Acoular Development Team'
-__date__ = '27 January 2026'
+__date__ = '28 January 2026'
 __version__ = '26.01'
