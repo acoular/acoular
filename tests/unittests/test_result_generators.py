66f483ba30de240b91075f4210d53705eaebe480
Art Pelling <account@artpelling.de>
2026-08-04 15:39:36 +0200

    add tests (xfail)

--- tests/unittests/test_result_generators.py
+++ tests/unittests/test_result_generators.py
@@ -12,11 +12,6 @@ import numpy as np
 import pytest
 from pytest_cases import parametrize, parametrize_with_cases
 
-xfails = {
-    ac.BeamformerTime: 'Issue #525',
-    ac.IntegratorSectorTime: 'Issue #525',  # test case relies on BeamformerTime as source
-}
-
 
 # @given(num=st.integers(min_value=1, max_value=1000))
 # suppress health check (fixtures are not reset!)
@@ -34,9 +29,12 @@ def test_result_generator(obj, num):  # don't use default value if @parametrize 
     num : :class:`int`
         Number of samples to return by the generator
     """
-    for gen, reason in xfails.items():
-        if isinstance(obj, gen):
-            pytest.xfail(reason)
+    xfails = {
+        ac.BeamformerTime: 'Issue #525',
+        ac.IntegratorSectorTime: 'Issue #525',  # test case relies on BeamformerTime as source
+    }
+    if reason := xfails.get(type(obj)):
+        pytest.xfail(reason)
 
     blocks = get_result(obj, num=num, f=list)
     assert len(blocks) > 0, 'Generator did not yield any blocks.'
@@ -64,3 +62,76 @@ def test_result_generator(obj, num):  # don't use default value if @parametrize 
 
     result = np.concatenate(blocks, axis=0)
     assert result.shape[0] == obj.num_samples, f'Total samples {result.shape[0]} != {obj.num_samples}'
+
+
+def _time_samples_sources(generator, seen=None):
+    """Yield TimeSamples objects in a generator's source graph."""
+    if seen is None:
+        seen = set()
+    if id(generator) in seen:
+        return
+    seen.add(id(generator))
+
+    if isinstance(generator, ac.TimeSamples):
+        yield generator
+    source = getattr(generator, 'source', None)
+    if isinstance(source, ac.Generator):
+        yield from _time_samples_sources(source, seen)
+    for source in getattr(generator, 'sources', []):
+        if isinstance(source, ac.Generator):
+            yield from _time_samples_sources(source, seen)
+
+
+def _retained_blocks(generator):
+    first = next(generator)
+    expected = first.copy()
+    try:
+        second = next(generator)
+    except StopIteration:
+        return None
+    return first, expected, second
+
+
+@parametrize('num', [1])
+@parametrize_with_cases('obj', cases=Generators)
+def test_result_block_stability(obj, num):
+    """A retained block must not change when its generator advances."""
+    xfails = {
+        ac.IRFFT: 'Issue #654',
+        ac.IntegratorSectorTime: 'Issue #654',
+        ac.MaskedTimeOut: 'Issue #654',
+        ac.MovingPointSourceDipole: 'Issue #654',
+        ac.OctaveFilterBank: 'Issue #654',
+        ac.PointSource: 'Issue #654',
+        ac.PointSourceDipole: 'Issue #654',
+        ac.RFFT: 'Issue #654',
+        ac.TimeReverse: 'Issue #654',
+    }
+    if reason := xfails.get(type(obj)):
+        pytest.xfail(reason)
+
+    retained_blocks = get_result(obj, num=num, f=_retained_blocks)
+    if retained_blocks is None:
+        return
+    first, expected, second = retained_blocks
+
+    np.testing.assert_array_equal(first, expected)
+    assert not np.shares_memory(first, second)
+
+
+@parametrize('num', [1])
+@parametrize_with_cases('obj', cases=Generators)
+def test_result_does_not_modify_source_blocks(obj, num):
+    """Processors must not modify data owned by their input source."""
+    xfails = {
+        ac.Mixer: 'Issue #595',
+        ac.SourceMixer: 'Issue #595',
+    }
+    if reason := xfails.get(type(obj)):
+        pytest.xfail(reason)
+
+    source_data = [(source, np.array(source.data, copy=True)) for source in _time_samples_sources(obj)]
+    get_result(obj, num=num)
+
+    for source, expected in source_data:
+        np.testing.assert_array_equal(source.data, expected)
